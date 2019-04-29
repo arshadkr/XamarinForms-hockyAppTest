32007866b43e4cc5794605bd15c668d4c6b6d225
arshadkr <arshadkr@gmail.com>
2019-04-29 13:02:25 +0530

    te

--- HKTestapp.Android/MainActivity.cs
+++ HKTestapp.Android/MainActivity.cs
@@ -34,7 +34,7 @@ namespace HKTestapp.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-            //check
+            //check 
         }
     }
 }
