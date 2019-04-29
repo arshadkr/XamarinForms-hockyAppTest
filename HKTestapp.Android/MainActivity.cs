afdf2b19f7ae6154d383eeeeec54469709588a0a
arshadkr <arshadkr@gmail.com>
2019-04-29 12:58:50 +0530

    Initial check-in of module HKTestapp

---
+++ HKTestapp.Android/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using HockeyApp.Android;
+using HockeyApp.Android.Metrics;
+
+namespace HKTestapp.Droid
+{
+    [Activity(Label = "HKTestapp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        private const string hockyAppID = "36296c108b9c4561838d10f084e981b0";
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            CrashManager.Register(this, hockyAppID);
+            MetricsManager.Register(Application, hockyAppID);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ HKTestapp/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HKTestapp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+            var isCrashed = HockeyApp.CrashManager.DidCrashInLastSession;
+            Console.Write(isCrashed);
+        }
+
+        void Clicked_TrackMe(object sender, System.EventArgs e)
+        {
+            HockeyApp.MetricsManager.TrackEvent(
+    "arshad click Event",
+    new Dictionary<string, string> { { "time", DateTime.UtcNow.ToString() } },
+    new Dictionary<string, double> { { "valu", 1.0 } }
+  );
+        }
+
+        void Clicked_CrashMe(object sender, System.EventArgs e)
+        {
+            throw new Exception();
+        }
+    }
+}
